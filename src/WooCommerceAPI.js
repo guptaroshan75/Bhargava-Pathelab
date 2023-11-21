ba2eb0c5201b00bddc331b6f3a53675422cb9091
Roshan Gupta <121086423+guptaroshan75@users.noreply.github.com>
2023-11-21 15:25:08 +0530

    Update WooCommerceAPI.js

--- src/WooCommerceAPI.js
+++ src/WooCommerceAPI.js
@@ -1,7 +1,7 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const WooCommerce = new WooCommerceRestApi({
-    url: "http://teamwebdevelopers.com/bhargava/backend/",
+    url: "https://teamwebdevelopers.com/bhargava/backend/",
     consumerKey: 'ck_0181870ebd755dd5a78b498d9c9bbf4c05548824',
     consumerSecret: 'cs_3f915e502466098d8f317c38bf944478bf025e21',
     version: "wc/v3",
