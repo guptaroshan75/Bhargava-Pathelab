3513f104d601f9a4b905222efc972cdc9f665075
Roshan Gupta <121086423+guptaroshan75@users.noreply.github.com>
2023-11-21 15:24:52 +0530

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
 // export const API = "http://localhost/bhargava/wp-json/wp/v2" 
 // export const API = "http://localhost/bhargava-app/server/wp-json/wp/v2" 
-export const API = "http://teamwebdevelopers.com/bhargava/backend/wp-json/wp/v2" 
+export const API = "https://teamwebdevelopers.com/bhargava/backend/wp-json/wp/v2" 
